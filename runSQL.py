159677ce6fc5556a9dea67f569fe330f5f2f225e
Unknown <jeg6@hawaii.edu>
2017-02-21 10:45:59 -1000

    MultiThreaded Connection
    Modifying connectionThread for sql.

--- runSQL.py
+++ runSQL.py
@@ -11,6 +11,8 @@ from lib.MySQLGrammar.MySQLListener import MySQLListener
 from lib.MySQLGrammar.MySQLParser import MySQLParser
 from lib.SQLLoader import SQLLoader
 
+from connectionThread import connectionThread
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         clustername = sys.argv[1]
@@ -35,10 +37,10 @@ if __name__ == '__main__':
     numnodes = loader.cfg['numnodes']
 
     # # Grab the config for the catalog
-    c_driver = 'catalog.driver=',loader.cfg['catalog']['driver']
-    c_hostname = 'catalog.hostname=',loader.cfg['catalog']['hostname']
-    c_username = 'catalog.username=',loader.cfg['catalog']['username']
-    c_password = 'catalog.passwd=',loader.cfg['catalog']['passwd']
+    c_driver = loader.cfg['catalog']['driver']
+    c_hostname = loader.cfg['catalog']['hostname']
+    c_username = loader.cfg['catalog']['username']
+    c_password = loader.cfg['catalog']['passwd']
 
     catalog_info = {
         'driver': c_driver,
@@ -57,4 +59,39 @@ if __name__ == '__main__':
     sql_loader = SQLLoader()
     walker = ParseTreeWalker()
     walker.walk(sql_loader, sql_tree)
-    print(sql_loader.getSQL())
+    # print(sql_loader.getSQL())
+
+# List of dictionary objects that will hold the parsed config data.
+nodes = [dict() for x in range(int(numnodes))]
+
+for node in range(int(numnodes)):
+    nodes[node]['username'] = loader.cfg['catalog']['username']
+    nodes[node]['passwd'] = loader.cfg['catalog']['passwd']
+    nodes[node]['hostname'] = loader.cfg['catalog']['hostname']
+    nodes[node]['driver'] = loader.cfg['catalog']['driver']
+    nodes[node]['tablename'] = sql_loader.sql['tablename']
+    nodes[node]['database'] = 'node' + str(node+1)
+
+sqlname = sqlfile
+sqlopen = open(sqlname, 'r')
+sql = sqlopen.read()
+
+# For loop that generates a dictionary object containing the parameters
+# for a nodes connection then makes a connectionThread for each node.
+threads = list()
+try:
+    for idnum in range(int(numnodes)):
+        config = {
+            'user': nodes[idnum]['username'],
+            'password': nodes[idnum]['passwd'],
+            'host': nodes[idnum]['hostname'],
+            'database': 'node' + str(idnum+1)
+        }
+        threads.insert( -1, connectionThread(idnum+1, config, sql, nodes[idnum]['driver'], catalog_info) )
+
+    # For loop that runs each connectionThread.
+    for conn in threads:
+        conn.run()
+except KeyError as err:
+    print('INVALID FORMAT IN CLUSTERCONFIG: Expected ' + str(err))
+    print('FAILED TO EXECUTE DDL')
