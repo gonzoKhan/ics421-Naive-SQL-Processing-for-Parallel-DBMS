21bf983d158fe8b438a011fd88a0371feae79b00
Unknown <jeg6@hawaii.edu>
2017-02-22 21:16:41 -1000

    Complete Part 3
    The runSQL program loads the catalog config, it then parses the sqlfile, extracts the tablename, searches the catalog db for nodes with that table, creates node config files for each qualifying node and then executes a thread run the sql query.

--- runSQL.py
+++ runSQL.py
@@ -1,4 +1,7 @@
 import sys
+import re
+import mysql.connector
+
 from antlr4 import *
 from antlr4.InputStream import InputStream
 from lib.ClusterConfigGrammar.ClusterConfigLexer import ClusterConfigLexer
@@ -11,8 +14,7 @@ from lib.MySQLGrammar.MySQLListener import MySQLListener
 from lib.MySQLGrammar.MySQLParser import MySQLParser
 from lib.SQLLoader import SQLLoader
 
-from connectionThread import connectionThread
-
+from lib.sqlConnect import SQLconnectionThread
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         clustername = sys.argv[1]
@@ -33,21 +35,6 @@ if __name__ == '__main__':
     loader = ClusterConfigLoader()
     walker = ParseTreeWalker()
     walker.walk(loader, cluster_tree)
-    # print(loader.getCFG())
-    numnodes = loader.cfg['numnodes']
-
-    # # Grab the config for the catalog
-    c_driver = loader.cfg['catalog']['driver']
-    c_hostname = loader.cfg['catalog']['hostname']
-    c_username = loader.cfg['catalog']['username']
-    c_password = loader.cfg['catalog']['passwd']
-
-    catalog_info = {
-        'driver': c_driver,
-        'hostname': c_hostname,
-        'username': c_username,
-        'password': c_password
-    }
 
     #Use antlr4 to parse sqlfile
     sql_input = FileStream(sqlfile)
@@ -61,37 +48,66 @@ if __name__ == '__main__':
     walker.walk(sql_loader, sql_tree)
     # print(sql_loader.getSQL())
 
-# List of dictionary objects that will hold the parsed config data.
-nodes = [dict() for x in range(int(numnodes))]
+    hostinfo = re.search('^.*//([\.\d]+):(\d+)/(.*)$', loader.cfg['catalog']['hostname'], flags=re.IGNORECASE)
 
-for node in range(int(numnodes)):
-    nodes[node]['username'] = loader.cfg['catalog']['username']
-    nodes[node]['passwd'] = loader.cfg['catalog']['passwd']
-    nodes[node]['hostname'] = loader.cfg['catalog']['hostname']
-    nodes[node]['driver'] = loader.cfg['catalog']['driver']
-    nodes[node]['tablename'] = sql_loader.sql['tablename']
-    nodes[node]['database'] = 'node' + str(node+1)
+    # # Grab the config for the catalog
+    c_driver = loader.cfg['catalog']['driver']
+    c_hostname = hostinfo.group(1)
+    c_username = loader.cfg['catalog']['username']
+    c_password = loader.cfg['catalog']['passwd']
 
-sqlname = sqlfile
-sqlopen = open(sqlname, 'r')
-sql = sqlopen.read()
+    catalog_info = {
+        'driver': c_driver,
+        'hostname': c_hostname,
+        'username': c_username,
+        'password': c_password
+    }
+
+    catalog_config = {
+        'user': c_username,
+        'password':c_password,
+        'host': hostinfo.group(1),
+        'port': hostinfo.group(2),
+        'database': hostinfo.group(3),
+    }
+    # print(catalog_config)
+
+    nodelist = dict()
+
+    nodesQuery = ("SELECT nodeid, nodeuser, nodepasswd, nodeurl, nodedriver FROM DTABLES WHERE tname = "'"{0}"'";")
+
+    # print (nodesQuery.format(sql_loader.sql['tablename']))
+    catalog = mysql.connector.connect(**catalog_config)
+    crsr = catalog.cursor()
+    crsr.execute(nodesQuery.format(sql_loader.sql['tablename']))
+    nodelist = crsr.fetchall()
+    numnodes = len(nodelist)
+
+    # print (catalog_info)
 
 # For loop that generates a dictionary object containing the parameters
 # for a nodes connection then makes a connectionThread for each node.
+
 threads = list()
+
 try:
-    for idnum in range(int(numnodes)):
-        config = {
-            'user': nodes[idnum]['username'],
-            'password': nodes[idnum]['passwd'],
-            'host': nodes[idnum]['hostname'],
-            'database': 'node' + str(idnum+1)
+    for x in range(numnodes):
+        nid, user, password, url, driver = nodelist[x]
+        nodehost = re.search('^.*//([\.\d]+):(\d+)/(.*)$', url, flags=re.IGNORECASE)
+        nodeconfig = {
+            'user': user,
+            'password': password,
+            'host': nodehost.group(1),
+            'port': nodehost.group(2),
+            'database': nodehost.group(3)
         }
-        threads.insert( -1, connectionThread(idnum+1, config, sql, nodes[idnum]['driver'], catalog_info) )
+        # print(nodeconfig)
+        threads.insert( -1, SQLconnectionThread(nid, nodeconfig, sqlfile, driver, catalog_info) )
 
     # For loop that runs each connectionThread.
     for conn in threads:
         conn.run()
+
 except KeyError as err:
     print('INVALID FORMAT IN CLUSTERCONFIG: Expected ' + str(err))
     print('FAILED TO EXECUTE DDL')
